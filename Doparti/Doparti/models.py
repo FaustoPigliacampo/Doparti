43534369f1016f3639539f7c922e4954000d3f6c
Fausto Pigliacampo <faustopiglia00@gmail.com>
2017-08-04 08:15:29 -0300

    af

--- Doparti/Doparti/models.py
+++ Doparti/Doparti/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Partido(models.Model):
+    fecha = models.DateField()
+    jugadores = models.IntegerField()
+    cancha = models.CharField(max_length = 30)
