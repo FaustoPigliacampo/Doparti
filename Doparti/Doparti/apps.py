cba53ad4b206659672258d88856ba0b0e356fb6b
thomasvelazquez <velazquezthomas7@gmail.com>
2017-11-03 08:28:23 -0300

    beta

--- Doparti/Doparti/apps.py
+++ Doparti/Doparti/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class PartidoyaConfig(AppConfig):
-    name = 'partidoya'
+class DopartiConfig(AppConfig):
+    name = 'Doparti'
--- Doparti/config/wsgi.py
+++ Doparti/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for proyecto6 project.
+WSGI config for Doparti project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto6.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
